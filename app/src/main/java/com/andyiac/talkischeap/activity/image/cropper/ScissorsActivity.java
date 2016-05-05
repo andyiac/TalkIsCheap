61913c5c96e725558c107adc8a2c4eda71801496
andyiac <andyiaczhang@gmail.com>
2016-05-05 14:47:47 +0800

    add scissors

---
+++ app/src/main/java/com/andyiac/talkischeap/activity/image/cropper/ScissorsActivity.java
@@ -0,0 +1,23 @@
+package com.andyiac.talkischeap.activity.image.cropper;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.andyiac.talkischeap.R;
+
+/**
+ * andyiac
+ * 16/5/5
+ */
+public class ScissorsActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.scissors_activity);
+
+    }
+}
