147a8dba2bde90cd93ff70249bc3a4f4bec59a04
andyiac <andyiaczhang@gmail.com>
2016-05-09 16:00:56 +0800

    softinputmode

--- app/src/main/java/com/andyiac/talkischeap/activity/SoftInputWithBottomWidgetActivity.java
+++ app/src/main/java/com/andyiac/talkischeap/activity/SoftInputWithBottomWidgetActivity.java
@@ -6,11 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import com.andyiac.talkischeap.R;
 
 /**
- * andyiac
- * 16/5/9
+ * 底部控件，随软键盘弹出，跟着上移
+ * 在AndroidManifast 中设置 如下就会使 底部的『我已阅读并同意小饭桌条款』随着键盘一起飘起来
+ * android:windowSoftInputMode="adjustResize
+ * <p/>
+ * andyiac  16/5/9
  */
-public class SoftInputWithBottomWidgetActivity extends AppCompatActivity {
 
+public class SoftInputWithBottomWidgetActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
