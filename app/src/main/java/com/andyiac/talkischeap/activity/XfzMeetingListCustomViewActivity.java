067fbac17569c050c48b07a7e3df613ea8b474fc
andyiac <andyiaczhang@gmail.com>
2016-06-02 10:35:44 +0800

    init xfz meeting custom view activity

---
+++ app/src/main/java/com/andyiac/talkischeap/activity/XfzMeetingListCustomViewActivity.java
@@ -0,0 +1,21 @@
+package com.andyiac.talkischeap.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.andyiac.talkischeap.R;
+
+/**
+ * andyiac
+ * 16/6/2
+ */
+
+public class XfzMeetingListCustomViewActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xfz_meeting_list_custom_view_activity);
+    }
+}
