167af594a09a772dfb761a206296a2926f77524b
andyiac <andyiaczhang@gmail.com>
2016-06-26 18:37:40 +0800

    add butter knife

--- app/src/main/java/com/andyiac/talkischeap/fragment/HomeFragment.java
+++ app/src/main/java/com/andyiac/talkischeap/fragment/HomeFragment.java
@@ -5,8 +5,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.andyiac.talkischeap.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * andyiac
@@ -14,6 +19,8 @@ import com.andyiac.talkischeap.R;
  */
 public class HomeFragment extends BaseFragment {
 
+
+    private Unbinder unbinder;
 
     public static HomeFragment newInstance(int instance) {
         Bundle args = new Bundle();
@@ -27,14 +34,29 @@ public class HomeFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.home_fragment, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
         return view;
     }
 
     @Override
     public void onStart() {
+
         super.onStart();
 
     }
 
+
+    @OnClick(R.id.id_home_test1)
+    public void test() {
+        Toast.makeText(getActivity(), "test click", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 
 }
